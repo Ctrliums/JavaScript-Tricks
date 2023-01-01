d083965e97516b770e306b7a9cd46d69e100f723
L4st Surviv0r <ehsan.shahbazicode@gmail.com>
2023-01-01 14:03:15 +0330

    Update 3-flatMethod.js

--- 3-flatMethod.js
+++ 3-flatMethod.js
@@ -1,3 +1,4 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 /*
 Array.prototype.flat()
 The flat() method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth.
@@ -8,8 +9,7 @@ const arr = [1, 2, 3, [4, 5, 6]];
 arr.flat();
 
 console.log(arr); // [1, 2, 3, 4, 5, 6]
-
-
+/////////////////////////////////////////////////////////////////////////////////////////////////////
 
 /*
 Using flat() on sparse arrays
